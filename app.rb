b7878484e62a0c1ad906e6409e6ea1316ea59609
Roman Pushkin <roman.pushkin@gmail.com>
2019-03-01 10:47:50 -0800

    Make it work

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ require 'ostruct'
 tests = [
   OpenStruct.new(id: :test1, time: 5),
   OpenStruct.new(id: :test2, time: 10),
-  OpenStruct.new(id: :test3, time: 3)
+  OpenStruct.new(id: :test3, time: 3),
+  OpenStruct.new(id: :test4, time: 1),
+  OpenStruct.new(id: :test5, time: 100)
 ]
 
 machines = [
@@ -17,6 +19,8 @@ heap = Heap.new
 machines.each do |machine|
   heap.push(machine)
 end
+
+tests = tests.sort { |a, b| b.time <=> a.time }
 
 tests.each do |test|
   machine = heap.pop
